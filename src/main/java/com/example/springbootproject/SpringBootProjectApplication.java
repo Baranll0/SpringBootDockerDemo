8c05adc1c7141c9073da13a11477ecaaeb99b82f
Baran Güçlü <gbaran180@gmail.com>
2024-04-30 12:06:39 +0300

    update project

---
+++ src/main/java/com/example/springbootproject/SpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
