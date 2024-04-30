97bde3c4afd8003f893fa532353036ca470dc30f
Baran Güçlü <gbaran180@gmail.com>
2024-04-30 14:21:25 +0300

    update docker

---
+++ src/main/java/com/example/springbootproject/entity/Customer.java
@@ -0,0 +1,49 @@
+package entity;
+
+public class Customer {
+    private long id;
+    private String name;
+    private int number;
+
+    public Customer() {
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", number=" + number +
+                '}';
+    }
+
+    public Customer(long id, String name, int number) {
+        this.id = id;
+        this.name = name;
+        this.number = number;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
