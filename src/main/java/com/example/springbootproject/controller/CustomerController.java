d8afb547d476c3db9799d2246b435abb8dd72af4
Baran Güçlü <gbaran180@gmail.com>
2024-05-01 12:15:16 +0300

    controller update

--- src/main/java/com/example/springbootproject/controller/CustomerController.java
+++ src/main/java/com/example/springbootproject/controller/CustomerController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class CustomerController {
     @GetMapping()
     public String home() {
-        return "Hello, World!";
+        return "Hello, Worldasfasd!";
     }
 }
