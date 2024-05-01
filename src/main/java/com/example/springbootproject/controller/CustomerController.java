e5076df608bd212d3a2f346f5eefb58280ae5520
Baran Güçlü <gbaran180@gmail.com>
2024-05-01 14:19:34 +0300

    commit controller

--- src/main/java/com/example/springbootproject/controller/CustomerController.java
+++ src/main/java/com/example/springbootproject/controller/CustomerController.java
@@ -2,11 +2,14 @@ package com.example.springbootproject.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/customer")
+
+@RestController
+@RequestMapping("/customer")
 public class CustomerController {
-    @GetMapping()
+    @GetMapping("/all")
     public String home() {
         return "BASLDKFJASDŞLFKJDS!";
     }
