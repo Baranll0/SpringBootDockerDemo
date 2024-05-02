35bad193b73604ede61f5b15276ef0d3a60180fa
Baran Güçlü <gbaran180@gmail.com>
2024-05-02 10:40:29 +0300

    ds

--- src/main/java/com/example/springbootproject/controller/CustomerController.java
+++ src/main/java/com/example/springbootproject/controller/CustomerController.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class CustomerController {
     @GetMapping("/all")
     public String home() {
-        return "Customer Pagedd!";
+        return "Customer Pasadlkasfd!";
     }
 }
