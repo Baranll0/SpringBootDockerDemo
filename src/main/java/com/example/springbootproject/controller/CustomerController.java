53b55aa1f83a9f801bfbe6bf0b529706f6a51710
Baran Güçlü <gbaran180@gmail.com>
2024-05-02 09:47:12 +0300

    customer paged

--- src/main/java/com/example/springbootproject/controller/CustomerController.java
+++ src/main/java/com/example/springbootproject/controller/CustomerController.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class CustomerController {
     @GetMapping("/all")
     public String home() {
-        return "Customer Page!";
+        return "Customer Pagedd!";
     }
 }
